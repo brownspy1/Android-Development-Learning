e4562a53d318e15d66edb8500f8fc6d929c46c36
brownspy1 <brownspy02@gmail.com>
2025-04-17 15:00:23 +0600

    Enhance app functionality
    - Add `AmolZikir` and `Developer_about` activities to the manifest.
    - Implement counter increment/decrement functionality in `MainActivity`.
    - Add "Go to Amol page" button with toast message to navigate to `AmolZikir` activity.
    - Add a text view to provide information to the user about the Amol page button.
    - Update the layout with a button and `TextView` elements.

--- DigitalTasbih/app/src/main/java/com/brownspy1/digitaltasbih/MainActivity.java
+++ DigitalTasbih/app/src/main/java/com/brownspy1/digitaltasbih/MainActivity.java
@@ -1,19 +1,23 @@
 package com.brownspy1.digitaltasbih;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import android.view.View;
+import android.widget.Toast;
+
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView add_btn;
-    ImageView reset_btn;
-    TextView counter;
+    ImageView add_btn,reset_btn;
+    TextView counter,hide_text;
+    Button go_to_next_page,developer;
     int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +30,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+
         counter = findViewById(R.id.counter);
         add_btn = findViewById(R.id.add_btn);
         reset_btn = findViewById(R.id.reset_btn);
+        go_to_next_page = findViewById(R.id.go_to_next_page);
+        hide_text = findViewById(R.id.hide_text);
 
+
+// this button for add tasbi count
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 counter.setText(String.valueOf(count));
             }
         });
-
+// for reset tasbih count
         reset_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // go to next activity / page with toest
+        go_to_next_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Going to Amol page",Toast.LENGTH_LONG).show();
+                hide_text.setVisibility(View.INVISIBLE);
+                Intent my_intent = new Intent(MainActivity.this,AmolZikir.class);
+                startActivity(my_intent);
+            }
+        });
 
     }
 }
