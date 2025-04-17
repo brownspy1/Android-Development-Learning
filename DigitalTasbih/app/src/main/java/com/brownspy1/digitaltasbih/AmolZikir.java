45a33962b23b4e6d34d148b52135a916d0aa9141
brownspy1 <brownspy02@gmail.com>
2025-04-17 15:46:13 +0600

    Enhance `AmolZikir` activity
    - Add a button to navigate to the `Developer_about` activity.
    - Implement a toast message to indicate the navigation to the developer page.
    - Add an `onClick` listener to the button for navigation.

--- DigitalTasbih/app/src/main/java/com/brownspy1/digitaltasbih/AmolZikir.java
+++ DigitalTasbih/app/src/main/java/com/brownspy1/digitaltasbih/AmolZikir.java
@@ -1,6 +1,10 @@
 package com.brownspy1.digitaltasbih;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AmolZikir extends AppCompatActivity {
-
+    Button developer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,17 @@ public class AmolZikir extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        developer = findViewById(R.id.developar);
+
+        developer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AmolZikir.this,"Going to developar page",Toast.LENGTH_LONG).show();
+                Intent developar_Page = new Intent(AmolZikir.this,Developer_about.class);
+                startActivity(developar_Page);
+            }
+        });
+
     }
 }
