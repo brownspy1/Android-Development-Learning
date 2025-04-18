9d15e68876552b563899594c00ddf3fa96623687
brownspy1 <brownspy02@gmail.com>
2025-04-19 02:14:08 +0600

    Add video playback and UI enhancements
    - Added `Video_play` activity to play videos using a `WebView`.
    - Implemented `rounded_bg.xml` for consistent UI elements.
    - Created `activity_video_play.xml` to display video and related information.
    - Implemented `background.jpg` to enhance UI background.
    - Added `video_link` variable for dynamic video loading.
    - Added various video UI to `activity_video_play.xml`.

---
+++ Technical_Jadughor/app/src/main/java/com/brownspy1/technicaljadughor/Video_play.java
@@ -0,0 +1,36 @@
+package com.brownspy1.technicaljadughor;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Video_play extends AppCompatActivity {
+    WebView webView = findViewById(R.id.webVideo);
+    public  static String video_link = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_video_play);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(video_link);
+
+
+
+
+
+    }
+}
