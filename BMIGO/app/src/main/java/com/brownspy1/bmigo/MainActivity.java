29e398f7c3153c687d14e11e485cc80cfe6f4ab5
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2025-05-16 00:43:45 +0600

    add

---
+++ BMIGO/app/src/main/java/com/brownspy1/bmigo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.brownspy1.bmigo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button getstarted;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        getstarted = findViewById(R.id.startBatton);
+
+        getstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent GoinputPage = new Intent(MainActivity.this, UserInput.class);
+                startActivity(GoinputPage);
+
+            }
+        });
+    }
+}
